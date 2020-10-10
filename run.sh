39b6b339a470e4347c5950e1862885dd6b52fa7b
serpent-charmer <zagirov.robert@yandex.ru>
2020-10-10 16:38:30 +0500

    fixed shell script

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
-python3 -V 2>1 1>/dev/null
+python3 -V 2>&1 1>/dev/null
 if [ $? -eq 0 ]
 then
-	python3 -c "import docx" 2>1 1>/dev/null
+	python3 -c "import docx" 2>&1 1>/dev/null
 	if [ $? -eq 0 ]
 	then
 		echo Есть docx
 	else
 		echo Устанавливаем docx
-		pip install python-docx 2>1 1>/dev/null
+		pip install python-docx 2>&1 1>/dev/null
 	fi
 	python3 makerrors.py
 else 
